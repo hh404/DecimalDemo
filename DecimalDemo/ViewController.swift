b5c219b0e124923b60052e2ad63de7fadfb267d8
Hans <huangjianwu@chinasofti.com>
2018-04-16 10:09:51 +0800

    first

--- DecimalDemo/ViewController.swift
+++ DecimalDemo/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let floatNumber :Double = 1100
+        let floatRate :Double = 1.1
+        let floatResult = floatNumber/floatRate
+        
+        //代表1.1
+        let ratingString = "0110"
+        let rateNumber :NSDecimalNumber = NSDecimalNumber(string: ratingString)
+        let rate :NSDecimalNumber = rateNumber.dividing(by: NSDecimalNumber(value: 100))
+        let amountDecimal = NSDecimalNumber(value: 1100)
+        let resultDecimal = amountDecimal.dividing(by: rate)
+        print(resultDecimal)
+        
+//        if(false){
+//            print("dd")
+//        }
     }
 
     override func didReceiveMemoryWarning() {
